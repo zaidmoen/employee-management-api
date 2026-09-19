a0d4330c79458603c9389e51e687891f321c32d2
Zaid Mayyalleh <164514360+zaidmoen@users.noreply.github.com>
2026-09-19 16:23:40 +0300

    db: add employee phone number migration

---
+++ employees/migrations/0002_employee_phone_number.py
@@ -0,0 +1,14 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [("employees", "0001_initial")]
+
+    operations = [
+        migrations.AddField(
+            model_name="employee",
+            name="phone_number",
+            field=models.CharField(blank=True, max_length=25),
+        ),
+    ]
+
